c7ba821eb1fe319feb66dd17f87e295c3678e3ba
Lauryn Gomez <lauryn@cmsllcnw.com>
2020-03-18 11:22:41 -0700

    deleted comments and fixed spacing and indentation References #1

--- pannier.rb
+++ pannier.rb
@@ -1,24 +1,14 @@
-
-
 class Pannier
 
   def initialize
-    @price = 3 # Set price. Flat rate per rental
+    @price = 3
   end
 
-
-  # Get price
   def price
     @price
   end
-
-  end
-
-
-
-  # def weight
-  #   @weight
-  # end
+  
+end
 
 
 
--- tail_pack.rb
+++ tail_pack.rb
@@ -1,11 +1,11 @@
 class TailPack
 
-def initialize
-  @cost = 2 # Flat rate per rental
-end
+  def initialize
+    @cost = 2 # Flat rate per rental
+  end
 
-def cost
-  @cost
-end
+  def cost
+    @cost
+  end
 
 end
